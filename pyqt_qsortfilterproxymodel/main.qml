8e806174052e8d5ed89a9bf0f9149bbf03581618
Fabian Sturm <f@rtfs.org>
2016-04-25 09:41:09 +0200

    FIrst sorting works.

--- pyqt_qsortfilterproxymodel/main.qml
+++ pyqt_qsortfilterproxymodel/main.qml
@@ -8,19 +8,6 @@ import SortFilterProxyModel 1.0
 
 ApplicationWindow {
 
-    function getCurrentIndex(list, element) {
-        console.log('getCurrentIndex')
-        if (list && element) {
-            for (var i = 0; i < list.length; i++) {
-                if (list[i].name === element.name) {
-                    console.log('Found item at pos: ' + i)
-                    return i
-                }
-            }
-        }
-        return -1
-    }
-
     id: mainWindow
     width: 800; height: 600
     color: "gray"
@@ -32,12 +19,17 @@ ApplicationWindow {
     SortFilterProxyModel {
         id: proxyModel
         source: mymodel
+
+        sortOrder: tableView.sortIndicatorOrder
+        //sortCaseSensitivity: Qt.CaseInsensitive
+        sortRole: tableView.getColumn(tableView.sortIndicatorColumn).role
     }
 
     TableView {
+        id: tableView
         anchors.fill: parent
-        //model: mymodel.items
         model: proxyModel
+        sortIndicatorVisible: true
         TableViewColumn {
             role: "name"
             title: "Name"
--- pyqt_qsortfilterproxymodel/mymodel.py
+++ pyqt_qsortfilterproxymodel/mymodel.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import pyqtProperty, pyqtSignal, pyqtSlot, QObject, QSortFilterProxyModel, QAbstractItemModel, QAbstractListModel, QModelIndex, Qt
+from PyQt5.QtCore import pyqtProperty, pyqtSignal, pyqtSlot, QObject, QVariant, QByteArray, QSortFilterProxyModel, QAbstractItemModel, QAbstractListModel, QModelIndex, Qt
 
 
 class SortFilterProxyModel(QSortFilterProxyModel):
@@ -16,6 +16,35 @@ class SortFilterProxyModel(QSortFilterProxyModel):
     def source (self, source):
         self.setSourceModel(source)
         self._source = source
+
+    @pyqtProperty(QByteArray)
+    def sortRole(self):
+        return self.roleNames().value(QSortFilterProxyModel.sortRole())
+
+    @sortRole.setter
+    def sortRole(self, role):
+        self.setSortRole(self.roleKey(role))
+
+    @pyqtProperty(Qt.SortOrder)
+    def sortOrder(self):
+        return self._sortOrder
+
+    @sortOrder.setter
+    def sortOrder(self, order):
+        self._sortOrder = order
+        self.sort(0, order);
+
+
+    def roleKey(self, role):
+        roles = self.roleNames()
+        print(roles)
+        for key, value in roles.items():
+            if value == role:
+                return key
+        return -1
+
+    def roleNames(self):
+        return self.sourceModel().roleNames()
 
 
 class MyItem(QObject):
